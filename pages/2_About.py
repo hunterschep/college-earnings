89411a9abf47753c0322fdd57e9ab37bb3dfcaee
scheppatphoto <67876717+hunterschep@users.noreply.github.com>
2024-07-15 19:12:01 -0700

    Fix github link

--- pages/2_About.py
+++ pages/2_About.py
@@ -17,7 +17,7 @@ st.markdown("---")
 # Contact and GitHub information with logos
 st.markdown(
     """
-    If there are issues with this application, please make a PR on the [GitHub repo](https://github.com/yourusername/college-earnings)
+    If there are issues with this application, please make a PR on the [GitHub repo](https://github.com/hunterschep/college-earnings)
     or reach out to me directly. Thank you for using this app! - Hunter
     """,
     unsafe_allow_html=True
@@ -28,7 +28,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("![GitHub](https://img.icons8.com/nolan/64/github.png)")
-    st.markdown("[GitHub Repository](https://github.com/yourusername/college-earnings)")
+    st.markdown("[GitHub Repository](https://github.com/hunterschep/college-earnings)")
 
 with col2:
     st.markdown("![LinkedIn](https://img.icons8.com/nolan/64/linkedin.png)")
